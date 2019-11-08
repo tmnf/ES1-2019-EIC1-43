1a9ba26ccfc53e357e52ed41234665c9f2a06c96
Tiago Farinha <tiagomnf1999@gmail.com>
2019-11-08 11:42:59 +0000

    FileUtils Added

---
+++ src/Utils/FileUtils.java
@@ -0,0 +1,20 @@
+package Utils;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+public class FileUtils {
+
+	public static String[] readFileRows(Sheet file) {
+
+	}
+
+	public static Cell getCellAt(Row row, int index) {
+
+	}
+
+	public static Cell getCellAtByText(Row row, String text) {
+
+	}
+}
