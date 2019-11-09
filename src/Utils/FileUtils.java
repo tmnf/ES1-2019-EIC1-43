bce9d76deb2070199b67c97a3390de56512bcc6c
mbtni-iscteiul <55883309+mbtni-iscteiul@users.noreply.github.com>
2019-11-09 23:58:21 +0000

    Update fileUtils.java
    cell.getAt() e CellToCompare() edit

--- src/Utils/FileUtils.java
+++ src/Utils/FileUtils.java
@@ -1,14 +1,27 @@
 package Utils;
 
 import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.Scanner;
 
+import org.apache.poi.ss.formula.FormulaParseException;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Comment;
+import org.apache.poi.ss.usermodel.Hyperlink;
+import org.apache.poi.ss.usermodel.RichTextString;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.util.CellAddress;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.util.SystemOutLogger;
 
 import GUI.MainWindow;
 
@@ -20,10 +33,8 @@ public class FileUtils {
 
 	public static Sheet readFile(String path) {
 		try {
-
 			workbook = WorkbookFactory.create(new File(path));
 			datatypeSheet = workbook.getSheetAt(0);
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -42,7 +53,7 @@ public class FileUtils {
 
 				while (cellIterator.hasNext()) {
 					Cell currentCell = cellIterator.next();
-
+					System.out.println(currentCell);
 					if (currentCell.getCellType() == CellType.STRING)
 						res += currentCell.getStringCellValue() + " :: ";
 					else if (currentCell.getCellType() == CellType.NUMERIC)
@@ -61,12 +72,33 @@ public class FileUtils {
 
 	// datatypeSheet = workbook.getSheetAt(0);
 
-//
-//	public static Cell getCellAt(Row row, int index) {
-//
-//	}
-//
-//	public static Cell getCellAtByText(Row row, String text) {
-//
-//	}
+	public static Cell getCellAt(Row row, int index) throws IOException {
+		datatypeSheet = workbook.getSheetAt(0);
+		Cell cell = null;
+		cell = row.getCell(index);
+		for (int i = 0; i < datatypeSheet.getLastRowNum(); i++) {
+			if (cell != null) {
+				System.out.println(cell + " - ");
+				// cell = row.getCell(i);
+			}
+		}
+		return cell;
+
+	}
+
+	public boolean CellToCompare(Cell c) {
+		datatypeSheet = workbook.getSheetAt(0);
+		for (Row r : datatypeSheet) {
+			for (int i = 0; i < datatypeSheet.getLastRowNum(); i++) {
+				if (r.getCell(i) != null && r.getCell(i).equals(c))
+					return true;
+			}
+		}
+		return false;
+	}
+
+	//
+	// public static Cell getCellAtByText(Row row, String text) {
+	//
+	// }
 }
