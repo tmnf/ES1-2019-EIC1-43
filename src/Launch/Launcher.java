82d134690ddae72c6cb7a6a07443ba3d61e90619
Tomás Santos <tfnss@iscte-iul.pt>
2019-11-10 00:35:36 +0000

    Launcher

--- src/Launch/Launcher.java
+++ src/Launch/Launcher.java
@@ -20,4 +20,38 @@ public class Launcher {
 		DataProcesser dp = DataProcesser.getInstance();
 		dp.initWindow();
 	}
+	public static String testFileReading() {
+		String res = "";
+		try {
+			//Workbook workbook = FileUtils.workbook;
+			//Sheet datatypeSheet = workbook.getSheetAt(0);
+			
+
+			Iterator<Row> iterator = FileUtils.datatypeSheet.iterator();
+
+			while (iterator.hasNext()) {
+				Row currentRow = iterator.next();
+				Iterator<Cell> cellIterator = currentRow.iterator();
+
+				while (cellIterator.hasNext()) {
+					Cell currentCell = cellIterator.next();
+
+					if (currentCell.getCellType() == CellType.STRING)
+						res += currentCell.getStringCellValue() + " : ";
+					else if (currentCell.getCellType() == CellType.NUMERIC)
+						res += currentCell.getNumericCellValue() + " : ";
+					else if (currentCell.getCellType() == CellType.BOOLEAN)
+						res += currentCell.getBooleanCellValue() + " : ";
+				}
+				res += "\n";
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+
+
 }
