0d0615de1b73776aca7ec73ff0fadf1aae63606a
Tiago Farinha <tmnfa@iscte-iul.pt>
2019-12-12 01:25:37 +0000

    Default Rule Change

--- src/JUnitTests/DefaultRuleTest.java
+++ src/JUnitTests/DefaultRuleTest.java
@@ -9,28 +9,22 @@ import Models.DefaultRule;
 
 public class DefaultRuleTest {
 
-	static Enums.Test test1;
-	static Enums.Test test2;
-
-	static DefaultRule dr1;
-
-	static String metricName;
+	private static DefaultRule dr1, dr2;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		test1 = Enums.Test.IPLASMA;
-		test2 = Enums.Test.IPLASMA;
-		dr1 = new DefaultRule(test1);
+		dr1 = new DefaultRule(Enums.Test.IPLASMA);
+		dr2 = new DefaultRule(Enums.Test.IPLASMA);
 	}
 
 	@Test
 	void testDefaultRule() {
-		assertEquals(test1, test2);
+		assertEquals(dr1.getTest(), dr2.getTest());
 	}
 
 	@Test
 	void testGetTest() {
-		assertEquals(test1, dr1.getTest());
+		assertEquals(Enums.Test.IPLASMA, dr1.getTest());
 	}
 
 	@Test
@@ -40,7 +34,7 @@ public class DefaultRuleTest {
 
 	@Test
 	void enumTestToString() {
-		assertEquals(dr1.getTest().getRealName(), test1.toString());
+		assertEquals(dr1.getTest().getRealName(), Enums.Test.IPLASMA.toString());
 	}
 
 }
